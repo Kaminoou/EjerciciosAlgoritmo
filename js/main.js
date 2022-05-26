8f335a4a60ebcacc8791363d6d7a7045ea1f5d9f
kaminoou <elisanti.armando@gmail.com>
2022-05-26 01:38:26 -0500

    Se hicieron correcciones de el ejercicio 5 y se utilizo parseFloat para las variables. Aún es versión Alpha

--- js/main.js
+++ js/main.js
@@ -1,5 +1,18 @@
 let comp = prompt("¿Quieres conocer el mayor, menor, múltiplo de 2, Mútiplo de 3 o si el 3° es la suma del 1° y el 2°?","mayor menor mul2 mul3 suma");
-let a,b,c;
+let a,b,c,res;
+function mayor(a,b,c){
+    if(a<b){//paso 1
+        if(b>c){//paso 2
+            alert(b+" es el mayor");
+        }else{
+            alert(c+" es el mayor");
+        }
+    }else if(a>c){//paso 3
+        alert(a+" es el mayor");
+    }else{
+        alert(c+" es el mayor");
+    }
+}
 
 switch (comp) {
 
@@ -7,46 +20,58 @@ switch (comp) {
 
     case "mayor":
         a=prompt("Ingresa el Pirmer Número", "");
-        if(a>100){
-            a=prompt("Que sea menor al 100, mijo :)", "");
+        if((a>100)||(a<1)){
+            a=prompt("Que sea entre 1 y 100, mijo :)", "");
         }
         b=prompt("Ingresa el Segundo Número", "");
-        if(b>100){
-            a=prompt("Que sea menor al 100, mijo :)", "");
+        if((b>100)||(b<1)){
+            b=prompt("Que sea entre 1 y 100, mijo :)", "");
         }
         c=prompt("Ingresa el Tercer Número", "");
-        if(c>100){
-            a=prompt("Que sea menor al 100, mijo :)", "");
+        if((c>100)||(c<1)){
+            c=prompt("Que sea entre 1 y 100, mijo :)", "");
         }
+        a=parseFloat(a);
+        b=parseFloat(b);
+        c=parseFloat(c);
+        console.log(typeof(a,b,c));
+        console.log(a,b,c);
+        mayor(a,b,c);
+
+        /*
         if(a<b){//paso 1
             if(b>c){//paso 2
-                alert(b+" es el mayor UNO");
+                alert(b+" es el mayor");
             }else{
-                alert(c+" es el mayor DOS");
+                alert(c+" es el mayor");
             }
         }else if(a>c){//paso 3
-            alert(a+" es el mayor TRES");
+            alert(a+" es el mayor");
         }else{
-            alert(c+" es el mayor FOUR");
+            alert(c+" es el mayor");
         }
+        */
     break;
 
 // 2.-Solicitar 3 números (entre el 1 y el 100)  y definir el menor de tres números
 
     case "menor":
+        alert("No eres Michael Jackson, ¿Verdad...?");
         a=prompt("Ingresa el Pirmer Número", "");
-        if(a>100){
-            a=prompt("Que sea menor al 100, mijo :)", "");
+        if((a>100)||(a<1)){
+            a=prompt("Que sea entre 1 y 100, mijo :)", "");
         }
         b=prompt("Ingresa el Segundo Número", "");
-        if(b>100){
-            a=prompt("Que sea menor al 100, mijo :)", "");
+        if((b>100)||(b<1)){
+            b=prompt("Que sea entre 1 y 100, mijo :)", "");
         }
         c=prompt("Ingresa el Tercer Número", "");
-        if(c>100){
-            a=prompt("Que sea menor al 100, mijo :)", "");
+        if((c>100)||(c<1)){
+            c=prompt("Que sea entre 1 y 100, mijo :)", "");
         }
-        alert("No eres Michael Jackson, ¿Verdad...?");
+        a=parseFloat(a);
+        b=parseFloat(b);
+        c=parseFloat(c);
         if(a>b){//paso 1
             if(b>c){//paso 2
                 alert(c+" es el menor");
@@ -64,11 +89,12 @@ switch (comp) {
 
     case "mul3":
         a=prompt("Ingresa un número entero entre 100 y 200: ", "");
+        a=parseFloat(a);
         if((a<100)||(a>200)){
             a=prompt("Pon atención, paps. Te doy otra oportunidad: ", "");
         }
         if(a%3 == 0){
-            alert("El número "+a+" sí es múltiplo de 3");
+            alert("El número "+a+" SÍ es múltiplo de 3");
         }else{
             alert("El número "+a+" NO es múltiplo de 3");
         }
@@ -80,6 +106,32 @@ switch (comp) {
         a=prompt("Ingresa el Pirmer Número", "");
         b=prompt("Ingresa el Segundo Número", "");
         c=prompt("Ingresa el Tercer Número", "");
+        a=parseFloat(a);
+        b=parseFloat(b);
+        c=parseFloat(c);
+        if( (a>b) && (a>c) ){
+            res=b+c;
+            if(a==res){
+                alert(a+" es la suma de "+b+" + "+c);
+            }else{
+                alert(a+" NO es la suma de "+b+" + "+c);
+            }
+        }else if( (b>a) && (b>c) ){
+            res=a+c;
+            if(b==res){
+                alert(b+" es la suma de "+a+" + "+c);
+            }else{
+                alert(b+" NO es la suma de "+a+" + "+c);
+            }
+        }else if( (c>a) && (c>b) ){
+            res=a+b;
+            if(c==res){
+                alert(c+" es la suma de "+b+" + "+a);
+            }else{
+                alert(c+" NO es la suma de "+a+" + "+b);
+            }
+        }    
+        /*
         if(a>b){//Paso 1
             console.log("Paso 1");
             if(a>c){//paso 3
@@ -104,17 +156,18 @@ switch (comp) {
                 
             }    
         }
-        console.log("En algo la cagaste");    
+        */    
         break;
 
 //6.-Elabora un algoritmo para leer un número y determinar si es par o impar
         
         case "mul2":
             a=prompt("Ingresa un número: ", "");
+            a=parseFloat(a);
             if(a%2 == 0){
-                alert("El número "+a+" es par");
+                alert("El número "+a+" SÍ es múltiplo de 2");
             }else{
-                alert("El número "+a+" NO es par");
+                alert("El número "+a+" NO es múltiplo de 2");
             }
         break;
 
